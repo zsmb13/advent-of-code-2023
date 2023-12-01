68e2cc92c9df4e4c1e53ff66b99c364217cf7c52
Márton Braun <braunmarci@gmail.com>
2023-12-01 09:59:04 +0100

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val first = line.first { it.isDigit() }
+            val last = line.last { it.isDigit() }
+            (first - '0') * 10 + (last - '0')
+        }
     }
+
+    val digits = listOf(
+            "some-value-that-does-not-occur-in-the-input",
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    )
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            fun getDigitValue(index: Int): Int {
+                val spelled = line.substring(index, index + 2)
+                return digits.indexOfFirst { it.startsWith(spelled) }
+            }
+
+            val firstNumericalDigit = line.indexOfFirst { it.isDigit() }
+            val firstSpelledOutDigit = line.indexOfAny(digits)
+
+            val first = if (firstSpelledOutDigit == -1 || (firstNumericalDigit != -1 && firstNumericalDigit < firstSpelledOutDigit)) {
+                line[firstNumericalDigit] - '0'
+            } else {
+                getDigitValue(firstSpelledOutDigit)
+            }
+
+            val lastNumericalDigit = line.indexOfLast { it.isDigit() }
+            val lastSpelledOutDigit = line.lastIndexOfAny(digits)
+
+            val last = if ((lastSpelledOutDigit == -1) || (lastNumericalDigit != -1 && lastNumericalDigit > lastSpelledOutDigit)) {
+                line[lastNumericalDigit] - '0'
+            } else {
+                val spelled = line.substring(lastSpelledOutDigit, lastSpelledOutDigit + 2)
+                getDigitValue(lastSpelledOutDigit)
+            }
+
+            first * 10 + last
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
 }
